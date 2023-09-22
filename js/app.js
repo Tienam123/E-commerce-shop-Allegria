a236dcfaff298190ba6f6b70de8205e777e941c2
Vladyslav <dr.tienam123@gmail.com>
2023-09-22 08:09:38 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -98,6 +98,31 @@
             }));
         }
     }), 0);
+    const burgerFunction = () => {
+        const mobileMenu = document.querySelector(".js-menu-container");
+        const openMenuBtn = document.querySelector(".js-open-menu");
+        const closeMenuBtn = document.querySelector(".js-close-menu");
+        const toggleMenu = () => {
+            const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+            openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+            mobileMenu.classList.toggle("is-open");
+            if (mobileMenu.classList.contains("is-open")) {
+                closeMenuBtn.classList.remove("hide");
+                openMenuBtn.classList.add("hide");
+            } else {
+                closeMenuBtn.classList.add("hide");
+                openMenuBtn.classList.remove("hide");
+            }
+        };
+        openMenuBtn.addEventListener("click", toggleMenu);
+        closeMenuBtn.addEventListener("click", toggleMenu);
+        window.matchMedia("(min-width: 768px)").addEventListener("change", (e => {
+            if (!e.matches) return;
+            mobileMenu.classList.remove("is-open");
+            openMenuBtn.setAttribute("aria-expanded", false);
+        }));
+    };
+    burgerFunction();
     window["FLS"] = true;
     isWebp();
     addTouchClass();
