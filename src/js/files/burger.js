cfc11034325eec6e80d4e1d9fa26ded1f17644aa
Vladyslav <dr.tienam123@gmail.com>
2023-09-22 08:07:27 +0300

    added style to the media from 991 to 1200

---
+++ src/js/files/burger.js
@@ -0,0 +1,29 @@
+export const burgerFunction = () => {
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+    if (mobileMenu.classList.contains("is-open")) {
+      closeMenuBtn.classList.remove("hide");
+      openMenuBtn.classList.add("hide");
+    } else {
+      closeMenuBtn.classList.add("hide");
+      openMenuBtn.classList.remove("hide");
+    }
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+  });
+};
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,5 @@
 // Подключение функционала "Чертогов Фрилансера"
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { burgerFunction } from "./burger.js";
+burgerFunction();
