bc72378361c21d088aa6bfc52518f52d0693d3ef
Vladyslav <dr.tienam123@gmail.com>
2023-09-25 08:27:47 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -223,8 +223,25 @@
         }));
     };
     const accordeon = () => {
-        const button = document.querySelector(".products-list__button");
-        console.log(button);
+        const button = document.querySelectorAll(".products-list__wrapper");
+        const productList = document.querySelectorAll(".products-list__item");
+        button.forEach((item => item.addEventListener("click", (() => {
+            const activeContent = document.querySelector("#" + item.dataset.tab);
+            if (activeContent.classList.contains("active")) {
+                activeContent.classList.remove("active");
+                item.classList.remove("active");
+                activeContent.style.maxHeight = 0;
+            } else {
+                productList.forEach((element => {
+                    element.classList.remove("active");
+                    element.style.maxHeight = 0;
+                }));
+                button.forEach((element => element.classList.remove("active")));
+                item.classList.add("active");
+                activeContent.classList.add("active");
+                activeContent.style.maxHeight = activeContent.scrollHeight + "px";
+            }
+        }))));
     };
     burgerFunction();
     accordeon();
